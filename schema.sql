0bc6bff733694b3767ec04d9d6b24d748e5097c5
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-04-08 16:17:01 +0300

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE swap (id SERIAL PRIMARY KEY, date TEXT, start_time TEXT, end_time TEXT, post TEXT, comment TEXT);
 CREATE TABLE take (id SERIAL PRIMARY KEY, date TEXT, start_time TEXT, end_time TEXT, post TEXT, comment TEXT);
 CREATE TABLE give (id SERIAL PRIMARY KEY, date TEXT, comment TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
