271bd86f5aaf7bff27128e9461ac5ab8ea225b7f
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-04-26 19:12:02 +0300

    updated db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from os import getenv
 
 # MUISTA MUUTTAA SECRET KEY JA DB_URL ENNEN HEROKUUN PUSHAAMISTA!!!!!!
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL", "postgresql://maijapajumaa1@localhost")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-app.secret_key = getenv("SECRET_KEY", "a0b66cfd66233307e11b6c30633acc5c")
+app.secret_key = getenv("SECRET_KEY")
