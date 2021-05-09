470f04627a6dc1d4fa9da9898c889ccce88a1685
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-05-09 16:28:37 +0300

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, type TEXT);
-CREATE TABLE swap (id SERIAL PRIMARY KEY, date TEXT, start_time TEXT, end_time TEXT, post TEXT, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
-CREATE TABLE take (id SERIAL PRIMARY KEY, date TEXT, start_time TEXT, end_time TEXT, post TEXT, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
-CREATE TABLE give (id SERIAL PRIMARY KEY, date TEXT, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
+CREATE TABLE swap (id SERIAL PRIMARY KEY, date DATE, start_time TEXT, end_time TEXT, post TEXT, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
+CREATE TABLE take (id SERIAL PRIMARY KEY, date DATE, start_time TEXT, end_time TEXT, post TEXT, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
+CREATE TABLE give (id SERIAL PRIMARY KEY, date DATE, comment TEXT, username TEXT REFERENCES users(username), visibility INTEGER);
 CREATE TABLE give_offers (id SERIAL PRIMARY KEY, give_id INTEGER REFERENCES give(id), offer TEXT, username TEXT REFERENCES users(username), date DATE, admin_visibility INTEGER, user_visibility INTEGER);
 CREATE TABLE take_offers (id SERIAL PRIMARY KEY, take_id INTEGER REFERENCES take(id), username TEXT REFERENCES users(username), admin_visibility INTEGER, user_visibility INTEGER);
 CREATE TABLE swap_offers (id SERIAL PRIMARY KEY, swap_id INTEGER REFERENCES swap(id), username TEXT REFERENCES users(username), offer TEXT, date DATE, admin_visibility INTEGER, user_visibility INTEGER);
